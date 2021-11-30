c3eb33ff15b635926935c27bb036ac84df8ff4f4
Mariana Gómez Betancourt <marisgob777@hotmail.com>
2021-11-29 21:34:21 -0500

    Comunicación Fragment Lista y detalle

--- sprint_3/app/src/main/java/com/example/recorridocafetero/detail/DetailFragment.kt
+++ sprint_3/app/src/main/java/com/example/recorridocafetero/detail/DetailFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.recorridocafetero.databinding.FragmentDetailBinding
 
 
 class DetailFragment : Fragment() {
     private lateinit var detailBinding: FragmentDetailBinding
+    private val args: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,6 +20,22 @@ class DetailFragment : Fragment() {
 
         detailBinding = FragmentDetailBinding.inflate(inflater, container, false)
         return detailBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val lugar = args.lugar
+        with(detailBinding){
+            nombreTextView.text = lugar.nombre
+            detalleTextView.text = lugar.detalle
+            direccionTextView.text = lugar.ubicacion
+            horaTextView.text = lugar.horario
+            numeroTextView.text = lugar.telefono
+            webTextView.text = lugar.paginaWeb
+            com.squareup.picasso.Picasso.get().load(lugar.urlDetalle).into(photoImageView)
+
+
+        }
     }
 
 
