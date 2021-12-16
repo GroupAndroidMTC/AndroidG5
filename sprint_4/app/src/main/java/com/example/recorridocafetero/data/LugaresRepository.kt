9736ce5eb9ca16fc14a643908d234e844a4ee1c4
Mariana Gómez Betancourt <marisgob777@hotmail.com>
2021-12-15 22:01:32 -0500

    Se elimina carpeta sprint 4

--- sprint_4/app/src/main/java/com/example/recorridocafetero/data/LugaresRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.recorridocafetero.data
-
-class LugaresRepository {
-
-    suspend fun getLugares() = ApiFactory.retrofit.getLugares()
-}
--- sprint_4/app/src/main/java/com/example/recorridocafetero/model/Lugar.kt
+++
@@ -1,4 +0,0 @@
-package com.example.recorridocafetero.model
-
-
-class Lugar : ArrayList<LugarItem>()
